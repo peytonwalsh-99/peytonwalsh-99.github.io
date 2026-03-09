9d8e1c42a98550f844b62f5566c67abd4116c539
Peyton Walsh <peytonwalsh222@gmail.com>
2026-03-09 14:50:49 -0400

    commiting today's location code

--- projects/game/game.js
+++ projects/game/game.js
@@ -17,8 +17,10 @@ function livingRoom() {
         "\n\tEntry Way\n\tDining Room");
     
     function processInput(input){
-        if (input.toLowerCase() === "Dining Room") {
-            diningRoom();
+        if (input.toLowerCase() === "dining room") {
+            diningRoom();}
+        else if (input.toLowerCase() === "entry way") {
+            entryWay();
         } else {
             stayHere();
             waitThenCall(livingRoom);
@@ -34,8 +36,10 @@ function diningRoom() {
         "\n\tLiving Room\n\tKitchen");
     
     function processInput(input){
-        if (input.toLowerCase() === "Living Room") {
-            livingRoom();
+        if (input.toLowerCase() === "living room") {
+            livingRoom();}
+        else if (input.toLowerCase() === "kitchen") {
+            kitchen();
         } else {
             stayHere();
             waitThenCall(diningRoom);
@@ -44,14 +48,65 @@ function diningRoom() {
     waitForInput(processInput);
 }
 
+function kitchen() {
+    clear();
+    print("\nYou are in the kitchen!");
+    print("\nWhere do you want to go next? Say one of these choices:" +
+        "\n\tDining Room");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "dining room") {
+            diningRoom();
+        } else {
+            stayHere();
+            waitThenCall(kitchen);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function entryWay() {
+    clear();
+    print("\nYou are in the entryway!");
+    print("\nWhere do you want to go next? Say one of these choices:" +
+        "\n\tLiving Room\n\tOffice");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "living room") {
+            livingRoom();}
+        if (input.toLowerCase() === "office") {
+            office();
+        } else {
+            stayHere();
+            waitThenCall(entryWay);
+        }
+    }
+    waitForInput(processInput);
+}
+function office() {
+    clear();
+    print("\nYou are in the office!");
+    print("\nWhere do you want to go next? Say one of these choices:" +
+        "\n\tEntry Way");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "entry way") {
+            entryWay();
+        } else {
+            stayHere();
+            waitThenCall(office);
+        }
+    }
+    waitForInput(processInput);
+}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
     print("Welcome to my game! Press any key to start");
-
+}
     function processInput(input){
             livingRoom();
     }
     waitForInput(processInput);
-}
+
