16e1838a74eb8e7471bb5c848ac316abf5fb1cc3
Peyton Walsh <peytonwalsh222@gmail.com>
2026-03-13 14:50:17 -0400

    organizing code and adding comments

--- projects/game/game.js
+++ projects/game/game.js
@@ -4,20 +4,17 @@ let gameActive = true; //this variable is required.
 //Declare your other global variables here
 let foundBowl1=false;
 let foodBowl2=false;
-let bowlFull=false;
-
-//If you need, add any "helper" functions here
+let bowlFull=false;//All global variables needed
 
 
-//Make one function for each location
-function livingRoom() {
-    clear();
+function livingRoom() {//function for living room
+    clear();//clean previous text
     print("\nYou are in the living room!");
     print("\nYou are sitting on the couch when you hear two loud dogs bounding into the room. It is 6 o'clock in the evening, according to them, dinner time.");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tEntry Way\n\tDining Room");
+        "\n\tEntry Way\n\tDining Room");//printing needed text
     
-    function processInput(input){
+    function processInput(input){//allows browser to process imput
         if (input.toLowerCase() === "dining room") {
             diningRoom();}
         else if (input.toLowerCase() === "entry way") {
@@ -28,15 +25,15 @@ function livingRoom() {
         }
     }
     waitForInput(processInput);
-}
+}//close funtion
 
-function diningRoom() {
-    clear();
+function diningRoom() {//open dining room funtions
+    clear();//clear previous text
     print("\nYou are in the dining room");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tLiving Room\n\tKitchen");
+        "\n\tLiving Room\n\tKitchen");//print needed text
     
-    function processInput(input){
+    function processInput(input){//allow browser to process input
         if (input.toLowerCase() === "living room") {
             livingRoom();}
         else if (input.toLowerCase() === "kitchen") {
@@ -54,22 +51,20 @@ function diningRoom() {
 	print("\nYou pick it up.");
 	}else{
 	print("You place 1 bowl on the floor. They are happy.");
-	}
+	}//allow text to update
 	
-}
+}//close funtion
 
-function kitchen() {
-    clear();
+function kitchen() {//open kitchen funtion
+    clear();//clear previous text
     print("\nYou are in the kitchen!");
     print("\nYou see a large container full of dog food.")
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tDining Room");
+        "\n\tDining Room");//print needed text
 
-   console.log(foundBowl1);
-   console.log(foundBowl2); 
 	if(foundBowl1==true && foundBowl2==true){
 	print("Now that you have both bowls, you may fill them and take them to their respective rooms. Type Fill Bowls to do this.");
-}
+}//allowing text to update
     function processInput(input){
         if (input.toLowerCase() === "dining room") {
             diningRoom();
@@ -84,14 +79,14 @@ function kitchen() {
         }
 	}
     waitForInput(processInput);
-}
+}//close function
 
-function entryWay() {
-    clear();
+function entryWay() {//open entry way function
+    clear();//clear previous text
     print("\nYou are in the entryway!");
     print("\nthere is nothing interesting in here except a pile of shoes sitting on the floor."); 
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tLiving Room\n\tOffice");
+        "\n\tLiving Room\n\tOffice");//print needed text
     
     function processInput(input){
         if (input.toLowerCase() === "living room") {
@@ -104,12 +99,12 @@ function entryWay() {
         }
     }
     waitForInput(processInput);
-}
-function office() {
-    clear();
+}//close function
+function office() {//open office function
+    clear();//clear previous text
     print("\nYou are in the office!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tEntry Way");
+        "\n\tEntry Way");//print needed text
     
     function processInput(input){
         if (input.toLowerCase() === "entry way") {
@@ -124,19 +119,17 @@ function office() {
 	if(bowlFull==false){
 	print("You see a shiny silver bowl on the floor. It is flipped upside-down with the remaining food spilt everywhere.");
 	print("\nYou pick it up.");
-	}
+	}//allow text to update
 	else{
 	print("You place the bowl on the ground. They are happy.");
         print("\nYou did it. Yay. That is the end of my pathetic little game.")
-	}
+	}//allow text to update
 	
 	
 }
-//finally, make sure you customize this to tell it what should happen at the
-//very start. For this simple example, any input will bring you
-//to locationA
-function start(){
+function start(){//open starting screen
     print("Welcome to my game! Press any key to start");
+	//text for starting screen
 }
     function processInput(input){
             livingRoom();
