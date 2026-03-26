35de8b9f6310a3f83e4c56d111e556f2b9bf0f7a
peytonwalsh-99 <peytonwalsh222@gmail.com>
2026-03-26 14:47:03 -0400

    commiting loops classwork bc i misspelled git last commit

---
+++ practice/loops/loops.js
@@ -0,0 +1,48 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+//fill in code for canvas1 here
+for(let y=0; y<201;y+=100){
+	for(let x=0; x<701; x+=100){
+		ctx.fillRect(x, y, 65, 20);
+	}	
+}
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+for(let y=0; y<801;y+=100){
+	for(let x=0; x<701; x+=100){
+		if(y>x){
+	ctx.fillRect(x, y, 65, 20);
+		}
+	}	
+}
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+for(let y=0; y<801;y+=100){
+	for(let x=0; x<701; x+=100){
+		if(y>x || y<x){
+	ctx.fillRect(x, y, 65, 20);
+		}
+	}	
+}
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here
+
+for(let y=0; y<801;y+=200){
+	for(let x=0; x<701; x+=200){
+		ctx.fillRect(x, y, 100, 100);
+	}	
+}
+for(let y=100; y<801;y+=200){
+	for(let x=100; x<701; x+=200){
+		ctx.fillRect(x, y, 100, 100);
+	}	
+}
